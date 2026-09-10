554b0df66ab0a18e4aeff9b169f6611dcabeb9af
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 00:26:46 +0530

    define AttendanceService contract

--- src/main/java/lk/SmartClass/service/AttendanceService.java
+++ src/main/java/lk/SmartClass/service/AttendanceService.java
@@ -1,4 +1,13 @@
 package lk.SmartClass.service;
 
+import lk.SmartClass.dto.request.AttendanceRequest;
+import lk.SmartClass.dto.response.AttendanceResponse;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public interface AttendanceService {
+    AttendanceResponse mark(AttendanceRequest request, String teacherUsername);
+    List<AttendanceResponse> getByEnrollment(Long enrollmentId);
+    List<AttendanceResponse> getByClassAndDate(Long classId, LocalDate date);
 }

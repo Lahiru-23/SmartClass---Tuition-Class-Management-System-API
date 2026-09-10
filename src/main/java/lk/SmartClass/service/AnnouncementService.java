73482388c5ae5eeaa22fda9aea57877d33385d4e
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 00:27:47 +0530

    define AnnouncementService contract

--- src/main/java/lk/SmartClass/service/AnnouncementService.java
+++ src/main/java/lk/SmartClass/service/AnnouncementService.java
@@ -1,4 +1,13 @@
 package lk.SmartClass.service;
 
+import lk.SmartClass.dto.request.AnnouncementRequest;
+import lk.SmartClass.dto.response.AnnouncementResponse;
+
+import java.util.List;
+
 public interface AnnouncementService {
+    AnnouncementResponse post(AnnouncementRequest request, String postedByUsername);
+    List<AnnouncementResponse> getByClass(Long classId);
+    List<AnnouncementResponse> getInstituteWide();
 }
+

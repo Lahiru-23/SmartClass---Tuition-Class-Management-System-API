3ea8c2153a2ab53034b7ff75bb44561d82076bc0
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 02:05:26 +0530

    add Assignment Response DTO

--- src/main/java/lk/SmartClass/dto/response/AssignmentResponse.java
+++ src/main/java/lk/SmartClass/dto/response/AssignmentResponse.java
@@ -1,4 +1,20 @@
 package lk.SmartClass.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
 public class AssignmentResponse {
+    private Long id;
+    private Long classId;
+    private String className;
+    private String title;
+    private String description;
+    private LocalDate dueDate;
+    private Integer maxMarks;
 }

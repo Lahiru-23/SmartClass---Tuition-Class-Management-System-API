c2a2e850296fafb4df4c08f8aa41a7e472de223e
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 02:29:31 +0530

    add Student Response DTO

--- src/main/java/lk/SmartClass/dto/response/StudentResponse.java
+++ src/main/java/lk/SmartClass/dto/response/StudentResponse.java
@@ -1,4 +1,20 @@
 package lk.SmartClass.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
 public class StudentResponse {
+    private Long id;
+    private String fullName;
+    private LocalDate dob;
+    private LocalDate enrollmentDate;
+    private Long guardianId;
+    private String guardianName;
+    private Long userId;
 }

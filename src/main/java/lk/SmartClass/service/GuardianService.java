237369435c07483f419e206a818c85fb5c44e414
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 00:25:37 +0530

    define GuardianService contract

--- src/main/java/lk/SmartClass/service/GuardianService.java
+++ src/main/java/lk/SmartClass/service/GuardianService.java
@@ -1,4 +1,14 @@
 package lk.SmartClass.service;
 
+import lk.SmartClass.dto.request.GuardianRequest;
+import lk.SmartClass.dto.response.GuardianResponse;
+
+import java.util.List;
+
 public interface GuardianService {
+    GuardianResponse create(GuardianRequest request);
+    GuardianResponse getById(Long id);
+    List<GuardianResponse> getAll();
+    GuardianResponse update(Long id, GuardianRequest request);
+    void delete(Long id);
 }

4f165af884878108397422eaea8ca12a16d1436e
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 02:47:49 +0530

    add Role Repository Spring Data JPA repository

--- src/main/java/lk/SmartClass/repository/RoleRepository.java
+++ src/main/java/lk/SmartClass/repository/RoleRepository.java
@@ -1,4 +1,10 @@
 package lk.SmartClass.repository;
 
-public interface RoleRepository {
+import lk.SmartClass.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
 }

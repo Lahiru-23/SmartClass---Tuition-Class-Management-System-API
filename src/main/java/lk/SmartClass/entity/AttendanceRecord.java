ecb2356781482d17b3445ed5b6511825004f545e
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-10 21:44:22 +0530

    add AttendanceRecord JPA entity and relationships

--- src/main/java/lk/SmartClass/entity/AttendanceRecord.java
+++ src/main/java/lk/SmartClass/entity/AttendanceRecord.java
@@ -1,4 +1,32 @@
 package lk.SmartClass.entity;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "attendance_records", uniqueConstraints = @UniqueConstraint(columnNames = {"enrollment_id", "date"}))
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class AttendanceRecord {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne @JoinColumn(name = "enrollment_id", nullable = false)
+    private Enrollment enrollment;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(nullable = false)
+    private String status;
+
+    @ManyToOne @JoinColumn(name = "marked_by", nullable = false)
+    private Teacher markedBy;
 }

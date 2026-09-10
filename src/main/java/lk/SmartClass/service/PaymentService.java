acc1aa5757e6ccb183caabf04580524ac626ef02
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 00:23:55 +0530

    define PaymentService contract

--- src/main/java/lk/SmartClass/service/PaymentService.java
+++ src/main/java/lk/SmartClass/service/PaymentService.java
@@ -1,4 +1,11 @@
 package lk.SmartClass.service;
 
+import lk.SmartClass.dto.request.PaymentRequest;
+import lk.SmartClass.dto.response.PaymentResponse;
+
+import java.util.List;
+
 public interface PaymentService {
+    PaymentResponse recordPayment(PaymentRequest request);
+    List<PaymentResponse> getByInvoice(Long invoiceId);
 }

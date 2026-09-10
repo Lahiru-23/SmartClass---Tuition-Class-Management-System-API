7bbedfed78f54f1d73eeb4afbbef916b91f545be
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 02:20:05 +0530

    add Notification Response DTO

--- src/main/java/lk/SmartClass/dto/response/NotificationResponse.java
+++ src/main/java/lk/SmartClass/dto/response/NotificationResponse.java
@@ -1,4 +1,18 @@
 package lk.SmartClass.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
 public class NotificationResponse {
+    private Long id;
+    private String message;
+    private String type;
+    private boolean isRead;
+    private LocalDateTime createdAt;
 }

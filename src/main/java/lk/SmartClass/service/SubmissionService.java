8469a670e21d21c80d183a67b00be8108ef7b003
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 00:23:05 +0530

    define SubmissionService contract

--- src/main/java/lk/SmartClass/service/SubmissionService.java
+++ src/main/java/lk/SmartClass/service/SubmissionService.java
@@ -1,4 +1,14 @@
 package lk.SmartClass.service;
 
+import lk.SmartClass.dto.request.GradeSubmissionRequest;
+import lk.SmartClass.dto.request.SubmissionRequest;
+import lk.SmartClass.dto.response.SubmissionResponse;
+
+import java.util.List;
+
 public interface SubmissionService {
+    SubmissionResponse submit(SubmissionRequest request);
+    SubmissionResponse grade(Long id, GradeSubmissionRequest request);
+    List<SubmissionResponse> getByAssignment(Long assignmentId);
+    List<SubmissionResponse> getByStudent(Long studentId);
 }

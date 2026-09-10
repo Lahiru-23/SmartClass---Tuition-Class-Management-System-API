063e3a71583867c90a655b011bde99db892bb30d
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 02:15:42 +0530

    add Invoice Response DTO

--- src/main/java/lk/SmartClass/dto/response/InvoiceResponse.java
+++ src/main/java/lk/SmartClass/dto/response/InvoiceResponse.java
@@ -1,4 +1,22 @@
 package lk.SmartClass.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
 public class InvoiceResponse {
+    private Long id;
+    private Long studentId;
+    private String studentName;
+    private BigDecimal amountDue;
+    private BigDecimal amountPaid;
+    private BigDecimal balance;
+    private LocalDate dueDate;
+    private String status;
 }

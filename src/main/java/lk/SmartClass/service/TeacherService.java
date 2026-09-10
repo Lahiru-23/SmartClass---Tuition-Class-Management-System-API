ffff3adddfa3a2831014d065b654c9f315a77575
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 00:22:21 +0530

    define TeacherService contract

--- src/main/java/lk/SmartClass/service/TeacherService.java
+++ src/main/java/lk/SmartClass/service/TeacherService.java
@@ -1,4 +1,14 @@
 package lk.SmartClass.service;
 
+import lk.SmartClass.dto.request.TeacherRequest;
+import lk.SmartClass.dto.response.TeacherResponse;
+
+import java.util.List;
+
 public interface TeacherService {
+    TeacherResponse create(TeacherRequest request);
+    TeacherResponse getById(Long id);
+    List<TeacherResponse> getAll();
+    TeacherResponse update(Long id, TeacherRequest request);
+    void delete(Long id);
 }

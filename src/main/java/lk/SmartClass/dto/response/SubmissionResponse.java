631c67ad2c80866f78cd7224a58c0851ab0f8a14
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 02:30:59 +0530

    add Submission Response DTO

--- src/main/java/lk/SmartClass/dto/response/SubmissionResponse.java
+++ src/main/java/lk/SmartClass/dto/response/SubmissionResponse.java
@@ -1,4 +1,25 @@
 package lk.SmartClass.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
 public class SubmissionResponse {
+    private Long id;
+    private Long assignmentId;
+    private String assignmentTitle;
+    private Long studentId;
+    private String studentName;
+    private LocalDateTime submittedAt;
+    private String fileUrl;
+    private Integer marksObtained;
+    private Integer maxMarks;
+    private String feedback;
+    private boolean late;
 }
+

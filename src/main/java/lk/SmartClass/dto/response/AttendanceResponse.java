acdb3809d824dd7bb61d558fc629950666e72e5c
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 02:07:02 +0530

    add Attendance Response DTO

--- src/main/java/lk/SmartClass/dto/response/AttendanceResponse.java
+++ src/main/java/lk/SmartClass/dto/response/AttendanceResponse.java
@@ -1,4 +1,19 @@
 package lk.SmartClass.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
 public class AttendanceResponse {
+    private Long id;
+    private Long enrollmentId;
+    private String studentName;
+    private LocalDate date;
+    private String status;
+    private String markedByName;
 }

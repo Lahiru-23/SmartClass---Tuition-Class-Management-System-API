f1478593b47a9f324932b5f967ea664afb4f2bbe
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 00:26:01 +0530

    define EnrollmentService contract

--- src/main/java/lk/SmartClass/service/EnrollmentService.java
+++ src/main/java/lk/SmartClass/service/EnrollmentService.java
@@ -1,4 +1,14 @@
 package lk.SmartClass.service;
 
+import lk.SmartClass.dto.request.EnrollmentRequest;
+import lk.SmartClass.dto.response.EnrollmentResponse;
+
+import java.util.List;
+
 public interface EnrollmentService {
+    EnrollmentResponse enroll(EnrollmentRequest request);
+    List<EnrollmentResponse> getByStudent(Long studentId);
+    List<EnrollmentResponse> getByClass(Long classId);
+    EnrollmentResponse updateStatus(Long id, String status);
+    void drop(Long id);
 }

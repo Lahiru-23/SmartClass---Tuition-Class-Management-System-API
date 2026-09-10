10d08d9df0011b7ec095a3cfe8b49b76a7bd73db
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 02:12:58 +0530

    add Guardian Request DTO

--- src/main/java/lk/SmartClass/dto/request/GuardianRequest.java
+++ src/main/java/lk/SmartClass/dto/request/GuardianRequest.java
@@ -1,4 +1,14 @@
 package lk.SmartClass.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class GuardianRequest {
+    @NotBlank(message = "Full name is required")
+    private String fullName;
+    @NotBlank(message = "Phone is required")
+    private String phone;
+    private String address;
+    private Long userId;
 }

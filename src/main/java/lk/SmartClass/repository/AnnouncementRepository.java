d7f830cb20d637486146250864ab7ee87799c0bc
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 03:06:02 +0530

    add Announcement Repository Spring Data JPA repository

--- src/main/java/lk/SmartClass/repository/AnnouncementRepository.java
+++ src/main/java/lk/SmartClass/repository/AnnouncementRepository.java
@@ -1,4 +1,12 @@
 package lk.SmartClass.repository;
 
-public interface AnnouncementRepository {
+import lk.SmartClass.entity.Announcement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AnnouncementRepository extends JpaRepository<Announcement, Long> {
+    List<Announcement> findByClassEntityIdOrderByPostedAtDesc(Long classId);
+    List<Announcement> findByClassEntityIsNullOrderByPostedAtDesc();
 }
+

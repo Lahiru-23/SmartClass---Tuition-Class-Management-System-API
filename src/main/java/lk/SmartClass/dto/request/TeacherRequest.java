91c90cbcac42cb839e6f545becd530566a597443
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 02:31:43 +0530

    add Teacher Request DTO

--- src/main/java/lk/SmartClass/dto/request/TeacherRequest.java
+++ src/main/java/lk/SmartClass/dto/request/TeacherRequest.java
@@ -1,4 +1,18 @@
 package lk.SmartClass.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class TeacherRequest {
+    @NotNull(message = "User ID is required")
+    private Long userId;
+    @NotBlank(message = "Full name is required")
+    private String fullName;
+    private String phone;
+    private String specialization;
+    private LocalDate hireDate;
 }

1ac9d830a8036021e961df32e2528680a97e244f
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-10 14:42:11 +0530

    cerate the springboot init

---
+++ src/main/java/lk/SmartClass/SmartClassApplication.java
@@ -0,0 +1,13 @@
+package lk.SmartClass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartClassApplication.class, args);
+	}
+
+}

28c4f3d2717ca8b03dcc2c5d80112d873cae1669
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 03:01:53 +0530

    add Notification Repository Spring Data JPA repository

--- src/main/java/lk/SmartClass/repository/NotificationRepository.java
+++ src/main/java/lk/SmartClass/repository/NotificationRepository.java
@@ -1,4 +1,11 @@
 package lk.SmartClass.repository;
 
-public interface NotificationRepository {
+import lk.SmartClass.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByUserIdOrderByCreatedAtDesc(Long userId);
+    List<Notification> findByUserIdAndIsReadFalse(Long userId);
 }

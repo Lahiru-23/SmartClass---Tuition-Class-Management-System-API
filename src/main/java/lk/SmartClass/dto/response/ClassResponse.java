ff67eeb1d9e9e655598a014b722fd325e57d3b5d
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 02:09:26 +0530

    add Class Response DTO

--- src/main/java/lk/SmartClass/dto/response/ClassResponse.java
+++ src/main/java/lk/SmartClass/dto/response/ClassResponse.java
@@ -1,4 +1,20 @@
 package lk.SmartClass.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
 public class ClassResponse {
+    private Long id;
+    private String subjectName;
+    private String teacherName;
+    private String termName;
+    private String name;
+    private Integer capacity;
+    private String mode;
+    private String room;
+    private int enrolledCount;
 }

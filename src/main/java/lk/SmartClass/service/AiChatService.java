055a0ad16cc651af7731960dd8092b5797a7b006
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 00:28:09 +0530

    define AiChatService contract

--- src/main/java/lk/SmartClass/service/AiChatService.java
+++ src/main/java/lk/SmartClass/service/AiChatService.java
@@ -1,4 +1,9 @@
 package lk.SmartClass.service;
 
+import lk.SmartClass.dto.response.AiChatResponse;
+
+import java.util.List;
+
 public interface AiChatService {
+    AiChatResponse chat(String message, String username, List<String> roles);
 }

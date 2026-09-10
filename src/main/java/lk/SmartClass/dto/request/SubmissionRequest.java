85c9bf516811b0603dd2765692fe131f2a0a7771
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 02:30:41 +0530

    add Submission Request DTO

--- src/main/java/lk/SmartClass/dto/request/SubmissionRequest.java
+++ src/main/java/lk/SmartClass/dto/request/SubmissionRequest.java
@@ -1,4 +1,11 @@
 package lk.SmartClass.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
 public class SubmissionRequest {
+    @NotNull(message = "Assignment ID is required") private Long assignmentId;
+    @NotNull(message = "Student ID is required") private Long studentId;
+    private String fileUrl;
 }

395f4adaef5e591022249a33336b598a6bc0f529
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 00:27:08 +0530

    define AssignmentService contract

--- src/main/java/lk/SmartClass/service/AssignmentService.java
+++ src/main/java/lk/SmartClass/service/AssignmentService.java
@@ -1,4 +1,14 @@
 package lk.SmartClass.service;
 
+import lk.SmartClass.dto.response.AssignmentResponse;
+import lk.SmartClass.dto.request.AssignmentRequest;
+
+import java.util.List;
+
 public interface AssignmentService {
+    AssignmentResponse create(AssignmentRequest request);
+    AssignmentResponse getById(Long id);
+    List<AssignmentResponse> getByClass(Long classId);
+    AssignmentResponse update(Long id, AssignmentRequest request);
+    void delete(Long id);
 }

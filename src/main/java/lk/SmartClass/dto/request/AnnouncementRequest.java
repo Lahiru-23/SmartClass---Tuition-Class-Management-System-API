0d8f00b83e1e4f294d0a62816bdb66caffd50732
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 02:03:41 +0530

    add Announcement Request DTO

--- src/main/java/lk/SmartClass/dto/request/AnnouncementRequest.java
+++ src/main/java/lk/SmartClass/dto/request/AnnouncementRequest.java
@@ -1,4 +1,11 @@
 package lk.SmartClass.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class AnnouncementRequest {
+    private Long classId;
+    @NotBlank(message = "Title is required") private String title;
+    @NotBlank(message = "Body is required") private String body;
 }

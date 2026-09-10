2c6f52b77b53f0701dca3ab7cf019dd6f7dda1af
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 03:04:28 +0530

    add Audit Log Repository Spring Data JPA repository

--- src/main/java/lk/SmartClass/repository/AuditLogRepository.java
+++ src/main/java/lk/SmartClass/repository/AuditLogRepository.java
@@ -1,4 +1,7 @@
 package lk.SmartClass.repository;
 
-public interface AuditLogRepository {
-}
+import lk.SmartClass.entity.AuditLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {}
+

d5a8ccc1fbaa7579c66924913049291db417eb2d
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-10 19:09:38 +0530

    configarations change and update

--- src/main/java/lk/SmartClass/config/WebConfig.java
+++ src/main/java/lk/SmartClass/config/WebConfig.java
@@ -1,4 +1,14 @@
 package lk.SmartClass.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/login.html");
+    }
 }

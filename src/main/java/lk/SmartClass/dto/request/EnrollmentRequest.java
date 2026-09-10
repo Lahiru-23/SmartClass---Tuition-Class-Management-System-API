d13557f2cecc0633e7e80454968025f0daaf2b0b
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 02:10:13 +0530

    add Enrollment Request DTO

--- src/main/java/lk/SmartClass/dto/request/EnrollmentRequest.java
+++ src/main/java/lk/SmartClass/dto/request/EnrollmentRequest.java
@@ -1,4 +1,10 @@
 package lk.SmartClass.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
 public class EnrollmentRequest {
+    @NotNull(message = "Student ID is required") private Long studentId;
+    @NotNull(message = "Class ID is required") private Long classId;
 }

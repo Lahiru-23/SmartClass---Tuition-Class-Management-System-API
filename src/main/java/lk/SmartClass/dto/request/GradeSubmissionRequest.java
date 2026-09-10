5d4c6a09a420b0e886f67069012782a50b5ba05a
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 02:12:24 +0530

    add Grade Submission Request DTO

--- src/main/java/lk/SmartClass/dto/request/GradeSubmissionRequest.java
+++ src/main/java/lk/SmartClass/dto/request/GradeSubmissionRequest.java
@@ -1,4 +1,13 @@
 package lk.SmartClass.dto.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
 public class GradeSubmissionRequest {
+    @NotNull(message = "Marks are required")
+    @Min(value = 0, message = "Marks cannot be negative")
+    private Integer marksObtained;
+    private String feedback;
 }

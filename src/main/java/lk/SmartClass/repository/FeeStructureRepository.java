fce937da93c6aa82361aa13ee3482db0166d6242
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 03:02:58 +0530

    add Fee Structure Repository Spring Data JPA repository

--- src/main/java/lk/SmartClass/repository/FeeStructureRepository.java
+++ src/main/java/lk/SmartClass/repository/FeeStructureRepository.java
@@ -1,4 +1,10 @@
 package lk.SmartClass.repository;
 
-public interface FeeStructureRepository {
+import lk.SmartClass.entity.FeeStructure;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FeeStructureRepository extends JpaRepository<FeeStructure, Long> {
+    List<FeeStructure> findByClassEntityId(Long classId);
 }

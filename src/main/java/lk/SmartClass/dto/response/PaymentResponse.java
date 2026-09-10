84b75a9f6f4109011d498e93416ad057b9ffee32
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 02:21:24 +0530

    add Payment Response DTO

--- src/main/java/lk/SmartClass/dto/response/PaymentResponse.java
+++ src/main/java/lk/SmartClass/dto/response/PaymentResponse.java
@@ -1,4 +1,21 @@
 package lk.SmartClass.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
 public class PaymentResponse {
+    private Long id;
+    private Long invoiceId;
+    private BigDecimal amountPaid;
+    private LocalDateTime paymentDate;
+    private String method;
+    private String receiptNo;
+    private String invoiceStatusAfterPayment;
 }

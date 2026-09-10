63a40647440bf113112ac6dcb82252b460481c64
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 02:15:18 +0530

    add Invoice Request DTO

--- src/main/java/lk/SmartClass/dto/request/InvoiceRequest.java
+++ src/main/java/lk/SmartClass/dto/request/InvoiceRequest.java
@@ -1,4 +1,14 @@
 package lk.SmartClass.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class InvoiceRequest {
+    @NotNull(message = "Student ID is required") private Long studentId;
+    @NotNull(message = "Fee structure ID is required") private Long feeStructureId;
+    @NotNull(message = "Due date is required") private LocalDate dueDate;
 }
+

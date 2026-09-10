5990b459d472ab455d86c7fa97086dcacad8a574
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 03:03:47 +0530

    add Class Schedule Repository Spring Data JPA repository

--- src/main/java/lk/SmartClass/repository/ClassScheduleRepository.java
+++ src/main/java/lk/SmartClass/repository/ClassScheduleRepository.java
@@ -1,4 +1,10 @@
 package lk.SmartClass.repository;
 
-public interface ClassScheduleRepository {
+import lk.SmartClass.entity.ClassSchedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ClassScheduleRepository extends JpaRepository<ClassSchedule, Long> {
+    List<ClassSchedule> findByClassEntityId(Long classId);
 }

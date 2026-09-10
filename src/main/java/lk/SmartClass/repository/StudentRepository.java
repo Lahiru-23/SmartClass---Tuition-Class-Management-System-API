383941e83bd378878de8646037b6a78ded5bbc7a
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 03:01:14 +0530

    add Student Repository Spring Data JPA repository

--- src/main/java/lk/SmartClass/repository/StudentRepository.java
+++ src/main/java/lk/SmartClass/repository/StudentRepository.java
@@ -1,4 +1,14 @@
 package lk.SmartClass.repository;
 
-public interface StudentRepository {
+import lk.SmartClass.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    List<Student> findByGuardianId(Long guardianId);
+    boolean existsByUserId(Long userId);
+    Optional<Student> findByUserId(Long userId);
+    Optional<Student> findByUserUsername(String username);
 }

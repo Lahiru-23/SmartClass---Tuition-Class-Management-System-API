31c53e02f926e1c737110b6e8e46301d64111c96
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 03:04:50 +0530

    add Attendance Repository Spring Data JPA repository

--- src/main/java/lk/SmartClass/repository/AttendanceRepository.java
+++ src/main/java/lk/SmartClass/repository/AttendanceRepository.java
@@ -1,4 +1,13 @@
 package lk.SmartClass.repository;
 
-public interface AttendanceRepository {
+import lk.SmartClass.entity.AttendanceRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface AttendanceRepository extends JpaRepository<AttendanceRecord, Long> {
+    List<AttendanceRecord> findByEnrollmentId(Long enrollmentId);
+    List<AttendanceRecord> findByEnrollmentClassEntityIdAndDate(Long classId, LocalDate date);
+    boolean existsByEnrollmentIdAndDate(Long enrollmentId, LocalDate date);
 }

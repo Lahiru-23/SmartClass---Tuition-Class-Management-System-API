d673d108665615fbb7a2a08878ef03c29d4365b5
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 00:24:57 +0530

    define InvoiceService contract

--- src/main/java/lk/SmartClass/service/InvoiceService.java
+++ src/main/java/lk/SmartClass/service/InvoiceService.java
@@ -1,4 +1,13 @@
 package lk.SmartClass.service;
 
+import lk.SmartClass.dto.request.InvoiceRequest;
+import lk.SmartClass.dto.response.InvoiceResponse;
+
+import java.util.List;
+
 public interface InvoiceService {
+    InvoiceResponse create(InvoiceRequest request);
+    InvoiceResponse getById(Long id);
+    List<InvoiceResponse> getByStudent(Long studentId);
+    List<InvoiceResponse> getOverdue();
 }

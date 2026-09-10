3c03a124318779d76145fd0e8408e4b405d55ef3
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 03:05:35 +0530

    add Assignment Repository Spring Data JPA repository

--- src/main/java/lk/SmartClass/repository/AssignmentRepository.java
+++ src/main/java/lk/SmartClass/repository/AssignmentRepository.java
@@ -1,4 +1,10 @@
 package lk.SmartClass.repository;
 
-public interface AssignmentRepository {
+import lk.SmartClass.entity.Assignment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
+    List<Assignment> findByClassEntityId(Long classId);
 }

d8e78b0230b814d4ef4ab81e9eee08632efae3ac
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 00:24:24 +0530

    define NotificationService contract

--- src/main/java/lk/SmartClass/service/NotificationService.java
+++ src/main/java/lk/SmartClass/service/NotificationService.java
@@ -1,4 +1,12 @@
 package lk.SmartClass.service;
 
+import lk.SmartClass.dto.response.NotificationResponse;
+import org.springframework.security.core.Authentication;
+
+import java.util.List;
+
 public interface NotificationService {
+    List<NotificationResponse> getForUser(Long userId);
+    List<NotificationResponse> getUnreadForUser(Long userId);
+    void markAsRead(Long id, Authentication authentication);
 }

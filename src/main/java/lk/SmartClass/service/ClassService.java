20f739b62a0b8848093f2a0514e845f4526212aa
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 00:26:26 +0530

    define ClassService contract

--- src/main/java/lk/SmartClass/service/ClassService.java
+++ src/main/java/lk/SmartClass/service/ClassService.java
@@ -1,4 +1,14 @@
 package lk.SmartClass.service;
 
+import lk.SmartClass.dto.request.ClassRequest;
+import lk.SmartClass.dto.response.ClassResponse;
+
+import java.util.List;
+
 public interface ClassService {
+    ClassResponse create(ClassRequest request);
+    ClassResponse getById(Long id);
+    List<ClassResponse> getAll();
+    ClassResponse update(Long id, ClassRequest request);
+    void delete(Long id);
 }

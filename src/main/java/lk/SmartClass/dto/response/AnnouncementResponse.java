4bd5994c64d13955a2f949541ca0a0f39584edf2
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 02:04:06 +0530

    add Announcement Response DTO

--- src/main/java/lk/SmartClass/dto/response/AnnouncementResponse.java
+++ src/main/java/lk/SmartClass/dto/response/AnnouncementResponse.java
@@ -1,4 +1,20 @@
 package lk.SmartClass.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
 public class AnnouncementResponse {
+    private Long id;
+    private Long classId;
+    private String className;
+    private String postedByUsername;
+    private String title;
+    private String body;
+    private LocalDateTime postedAt;
 }

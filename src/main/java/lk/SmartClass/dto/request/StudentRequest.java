75ecf24c1a494b0d00b512994eb8b7a9461df181
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 02:29:45 +0530

    add Student Request DTO

--- src/main/java/lk/SmartClass/dto/request/StudentRequest.java
+++ src/main/java/lk/SmartClass/dto/request/StudentRequest.java
@@ -1,4 +1,19 @@
 package lk.SmartClass.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class StudentRequest {
+    @NotBlank(message = "Full name is required")
+    private String fullName;
+    @Past(message = "Date of birth must be in the past")
+    private LocalDate dob;
+    private Long guardianId;
+
+    private Long userId;
 }
+

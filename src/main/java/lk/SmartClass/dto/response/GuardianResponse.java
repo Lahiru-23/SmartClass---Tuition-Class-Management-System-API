cbb063ab73ddbfdd8d33f1e5657d9eec28b80572
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 02:13:29 +0530

    add Guardian Response DTO

--- src/main/java/lk/SmartClass/dto/response/GuardianResponse.java
+++ src/main/java/lk/SmartClass/dto/response/GuardianResponse.java
@@ -1,4 +1,16 @@
 package lk.SmartClass.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
 public class GuardianResponse {
+    private Long id;
+    private String fullName;
+    private String phone;
+    private String address;
+    private Long userId;
 }

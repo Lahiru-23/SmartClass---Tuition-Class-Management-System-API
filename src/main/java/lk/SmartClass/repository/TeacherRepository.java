9a043700bba77420fce266d10da18c31f3203c50
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 02:48:38 +0530

    add Teacher Repository Spring Data JPA repository

--- src/main/java/lk/SmartClass/repository/TeacherRepository.java
+++ src/main/java/lk/SmartClass/repository/TeacherRepository.java
@@ -1,4 +1,12 @@
 package lk.SmartClass.repository;
 
-public interface TeacherRepository {
+import lk.SmartClass.entity.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+    Optional<Teacher> findByUserId(Long userId);
+    Optional<Teacher> findByUserUsername(String username);
+    boolean existsByUserId(Long userId);
 }

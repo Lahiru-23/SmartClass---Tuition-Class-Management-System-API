a2095955e99b83dc7719835460f548a92d9772e6
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 02:48:06 +0530

    add User Repository Spring Data JPA repository

--- src/main/java/lk/SmartClass/repository/UserRepository.java
+++ src/main/java/lk/SmartClass/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package lk.SmartClass.repository;
 
-public interface UserRepository {
+import lk.SmartClass.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }

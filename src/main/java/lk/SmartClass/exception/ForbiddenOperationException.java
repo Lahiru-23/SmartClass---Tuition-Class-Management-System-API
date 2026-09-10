3f1375720c4d2ff4232cbf9be8a42d5be1645be1
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-10 22:22:15 +0530

    add ForbiddenOperationException for 403 responses

---
+++ src/main/java/lk/SmartClass/exception/ForbiddenOperationException.java
@@ -0,0 +1,5 @@
+package lk.SmartClass.exception;
+
+public class ForbiddenOperationException extends RuntimeException {
+    public ForbiddenOperationException(String message) { super(message); }
+}

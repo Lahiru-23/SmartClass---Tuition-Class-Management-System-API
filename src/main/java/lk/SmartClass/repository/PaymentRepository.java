b0bcf23e925f1182875953a3e2a0c815e97a9092
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 03:01:39 +0530

    add Payment Repository Spring Data JPA repository

--- src/main/java/lk/SmartClass/repository/PaymentRepository.java
+++ src/main/java/lk/SmartClass/repository/PaymentRepository.java
@@ -1,4 +1,11 @@
 package lk.SmartClass.repository;
 
-public interface PaymentRepository {
+import lk.SmartClass.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    List<Payment> findByInvoiceId(Long invoiceId);
+    boolean existsByReceiptNo(String receiptNo);
 }

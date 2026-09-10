84e8994174f2b66507f155c59a4d321ff7db5f7e
Lahiru-23 <lahiruwaduthanthri2002@gmail.com>
2026-09-11 00:23:33 +0530

    define StudentService contract

--- src/main/java/lk/SmartClass/service/StudentService.java
+++ src/main/java/lk/SmartClass/service/StudentService.java
@@ -1,4 +1,14 @@
 package lk.SmartClass.service;
 
+import lk.SmartClass.dto.request.StudentRequest;
+import lk.SmartClass.dto.response.StudentResponse;
+
+import java.util.List;
+
 public interface StudentService {
+    StudentResponse create(StudentRequest request);
+    StudentResponse getById(Long id);
+    List<StudentResponse> getAll();
+    StudentResponse update(Long id, StudentRequest request);
+    void delete(Long id);
 }
